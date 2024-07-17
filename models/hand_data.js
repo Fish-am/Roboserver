afc99f7ad9ae84fad15a9306f393e25dd05ee1af
Fish-am <hisham.khan128@gmail.com>
2024-07-17 20:11:44 +0800

    hand_data.js

--- models/hand_data.js
+++ models/hand_data.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-const handDataSchema = new mongoose.Schema(
-  {},
-  { timestamps: true },
-  { strict: false }
-);
-// Create the model
-const HandData = mongoose.model("HandData", handDataSchema, "hand_data", {
-  dbName: "robotics_hand",
-});
 
-module.exports = HandData;
+const createModel = (dbName, collectionName) => {
+  const schema = new mongoose.Schema({}, { timestamps: true, strict: false });
+  return mongoose.model(collectionName, schema, collectionName, { dbName });
+};
+
+module.exports = createModel;
